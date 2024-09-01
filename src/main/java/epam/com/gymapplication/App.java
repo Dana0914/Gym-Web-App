fcdbce956c0e7eeb5f80fd4c74ee48140aa5c94c
Dana <Dana.ackerman0914@gmail.com>
2024-09-01 21:56:44 +0500

    Add main class

---
+++ src/main/java/epam/com/gymapplication/App.java
@@ -0,0 +1,16 @@
+package epam.com.gymapplication;
+
+
+import epam.com.gymapplication.config.AppConfig;
+import epam.com.gymapplication.postprocessor.StorageInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        StorageInitializer storageInitializer = context.getBean(StorageInitializer.class);
+        storageInitializer.initializeStorage();
+
+    }
+}
