21a04149454b119236429ed02218f0562accf57e
Dana <Dana.ackerman0914@gmail.com>
2024-12-04 15:14:09 +0500

    Add swagger config

--- src/main/java/epam/com/gymapplication/config/AppConfig.java
+++ src/main/java/epam/com/gymapplication/config/AppConfig.java
@@ -2,12 +2,13 @@ package epam.com.gymapplication.config;
 
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
@@ -16,9 +17,21 @@ public class AppConfig {
 
 
     @Bean
-    public HealthIndicator postgresTableSizeHealthIndicator(MeterRegistry meterRegistry, JdbcTemplate jdbcTemplate) {
+    public HealthIndicator postgresTableSizeHealthIndicator(MeterRegistry meterRegistry,
+                                                            JdbcTemplate jdbcTemplate) {
         return new DatabaseTableHealthIndicator(meterRegistry, jdbcTemplate);
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 
 
 
