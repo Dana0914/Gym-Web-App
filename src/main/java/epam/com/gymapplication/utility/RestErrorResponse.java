17723777db55ff795f2f8a49a731d024e4ad52a4
Dana <Dana.ackerman0914@gmail.com>
2024-12-04 15:16:52 +0500

    Updated ApiResponse class

--- src/main/java/epam/com/gymapplication/utility/RestErrorResponse.java
+++ src/main/java/epam/com/gymapplication/utility/RestErrorResponse.java
@@ -1,5 +1,54 @@
 package epam.com.gymapplication.utility;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
-public record RestErrorResponse(int status, String message, LocalDateTime timestamp) {}
+public class RestErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+    private Map<String, String> errors;
+
+    public RestErrorResponse() {
+
+    }
+
+    public RestErrorResponse(int status, String message, LocalDateTime timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+    public RestErrorResponse(int status, String message, LocalDateTime timestamp, Map<String, String> errors) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.errors = errors;
+    }
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+}
