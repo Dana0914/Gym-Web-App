198188485b2680d5b390e3acd99f62c214590b2b
Dana <Dana.ackerman0914@gmail.com>
2025-02-21 16:46:10 +0500

    Code refactor

--- src/main/java/epam/com/gymapplication/config/DatabaseConfig.java
+++ src/main/java/epam/com/gymapplication/config/DatabaseConfig.java
@@ -1,72 +1,74 @@
 package epam.com.gymapplication.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//import javax.sql.DataSource;
 
-import javax.sql.DataSource;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DatabaseConfig {
-    @Bean
-    @Profile("local")
-    public DataSource localDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/gym");
-        config.setUsername("postgres");
-        config.setPassword("postgres");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setMaximumPoolSize(10);
-        config.setConnectionTimeout(30000);
-        config.setIdleTimeout(600000);
-        config.setMaxLifetime(1800000);
-        return new HikariDataSource(config);
-    }
+//    @Bean
+//    @Profile("local")
+//    public DataSource localDataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl("jdbc:postgresql://postgres-db:5432/gym");
+//        config.setUsername("postgres");
+//        config.setPassword("postgres");
+//        config.setDriverClassName("org.postgresql.Driver");
+//        config.setMaximumPoolSize(10);
+//        config.setConnectionTimeout(30000);
+//        config.setIdleTimeout(600000);
+//        config.setMaxLifetime(1800000);
+//        return new HikariDataSource(config);
+//    }
 
-    @Bean
-    @Profile("dev")
-    public DataSource devDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://dev-server:5432/gym");
-        config.setUsername("postgres");
-        config.setPassword("postgres");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setMaximumPoolSize(10);
-        config.setConnectionTimeout(30000);
-        config.setIdleTimeout(600000);
-        config.setMaxLifetime(1800000);
-        return new HikariDataSource(config);
-    }
-
-    @Bean
-    @Profile("stg")
-    public DataSource stgDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://staging-server:5432/gym");
-        config.setUsername("postgres");
-        config.setPassword("postgres");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setMaximumPoolSize(10);
-        config.setConnectionTimeout(30000);
-        config.setIdleTimeout(600000);
-        config.setMaxLifetime(1800000);
-        return new HikariDataSource(config);
-    }
-
-    @Bean
-    @Profile("prod")
-    public DataSource prodDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://prod-server:5432/gym");
-        config.setUsername("postgres");
-        config.setPassword("postgres");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setMaximumPoolSize(10);
-        config.setConnectionTimeout(30000);
-        config.setIdleTimeout(600000);
-        config.setMaxLifetime(1800000);
-        return new HikariDataSource(config);
-    }
+//    @Bean
+//    @Profile("dev")
+//    public DataSource devDataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl("jdbc:postgresql://dev-server:5432/gym");
+//        config.setUsername("postgres");
+//        config.setPassword("postgres");
+//        config.setDriverClassName("org.postgresql.Driver");
+//        config.setMaximumPoolSize(10);
+//        config.setConnectionTimeout(30000);
+//        config.setIdleTimeout(600000);
+//        config.setMaxLifetime(1800000);
+//        return new HikariDataSource(config);
+//    }
+//
+//    @Bean
+//    @Profile("stg")
+//    public DataSource stgDataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl("jdbc:postgresql://staging-server:5432/gym");
+//        config.setUsername("postgres");
+//        config.setPassword("postgres");
+//        config.setDriverClassName("org.postgresql.Driver");
+//        config.setMaximumPoolSize(10);
+//        config.setConnectionTimeout(30000);
+//        config.setIdleTimeout(600000);
+//        config.setMaxLifetime(1800000);
+//        return new HikariDataSource(config);
+//    }
+//
+//    @Bean
+//    @Profile("prod")
+//    public DataSource prodDataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl("jdbc:postgresql://prod-server:5432/gym");
+//        config.setUsername("postgres");
+//        config.setPassword("postgres");
+//        config.setDriverClassName("org.postgresql.Driver");
+//        config.setMaximumPoolSize(10);
+//        config.setConnectionTimeout(30000);
+//        config.setIdleTimeout(600000);
+//        config.setMaxLifetime(1800000);
+//        return new HikariDataSource(config);
+//    }
 }
